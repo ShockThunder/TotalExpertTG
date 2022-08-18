9125931d4c8157579deb9655faf9147e8b882233
ShockThunder <skysidtim@gmail.com>
2022-08-18 16:48:48 +0200

    Add basic text generation

---
+++ TotalExpertTG/TotalExpertTG/Program.cs
@@ -0,0 +1,91 @@
+﻿
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace TotalExpertTG;
+
+public class Program
+{
+    public static TelegramBotClient bot = new(TOKEN);
+    public const string TOKEN = "5567107382:AAH1lCd-VCFMaGKbW_HqYdyiFQnIVNZH_Nk";
+    public const string MEME_CALLBACK = "memeCallback";
+
+    public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        Console.WriteLine(JsonSerializer.Serialize(update));
+        if(update.Type == UpdateType.Message)
+        {
+            var message = update.Message;
+            if (message.Text.ToLower() == "/start")
+            {
+                // var button = new KeyboardButton("Дай мем, эксперт.");
+                // var reply = new ReplyKeyboardMarkup(button);
+                var button = InlineKeyboardButton.WithCallbackData("Дай мем, эксперт.", MEME_CALLBACK);
+                var reply = new InlineKeyboardMarkup(
+                    new[]
+                    {
+                        new[]
+                        {
+                            button
+                        }
+                    });
+                await botClient.SendTextMessageAsync(message.Chat, "Чего тебе, неуч?", replyMarkup: reply);
+                return;
+            }
+            await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
+        }
+
+        if (update.Type == UpdateType.CallbackQuery)
+        {
+            var callback = update.CallbackQuery;
+            if (callback.Data == MEME_CALLBACK)
+            {
+                try
+                {
+                    var uri = new Uri("https://pbs.twimg.com/media/Ei8m_X0WkAAnRYg.jpg");
+                   
+                    await botClient.SendPhotoAsync(callback.Message.Chat, new InputOnlineFile(uri));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+
+    public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        // Некоторые действия
+        Console.WriteLine(JsonSerializer.Serialize(exception));
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+        // var cts = new CancellationTokenSource();
+        // var cancellationToken = cts.Token;
+        // var receiverOptions = new ReceiverOptions
+        // {
+        //     AllowedUpdates = { }, // receive all update types
+        // };
+        // bot.StartReceiving(
+        //     HandleUpdateAsync,
+        //     HandleErrorAsync,
+        //     receiverOptions,
+        //     cancellationToken
+        // );
+
+        var textAdder = new TextAdder();
+        var result = textAdder.GenerateMeme("text");
+        
+        Console.ReadLine();
+    }
+}
---
+++ TotalExpertTG/TotalExpertTG/TextAdder.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+
+namespace TotalExpertTG;
+
+public class TextAdder
+{
+    
+
+    public string GenerateMeme(string text)
+    {
+        var firstText = "Hello";
+        var secondText = "World";
+
+        var firstLocation = new PointF(10f, 10f);
+        var secondLocation = new PointF(10f, 50f);
+
+        var imageFilePath = @"resources\picture.bmp";
+        var path = Path.Combine(Directory.GetCurrentDirectory(), @"resources\FortniteExperts.png");
+        var bmp1 = new Bitmap(path);
+    
+    
+        using(Graphics graphics = Graphics.FromImage(bmp1))
+        {
+            using (Font arialFont =  new Font("Arial", 10))
+            {
+                graphics.DrawString(firstText, arialFont, Brushes.Blue, firstLocation);
+                graphics.DrawString(secondText, arialFont, Brushes.Red, secondLocation);
+            }
+        }
+
+        bmp1.Save(imageFilePath);//save the image file
+        return imageFilePath;
+    }
+}
